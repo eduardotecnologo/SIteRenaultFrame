b029c1d2ff6c8d4cc6e6c4df3dab56ff671b514b
eduardodeveloper <eduardotecnologo@hotmail.com>
2017-05-02 19:12:21 -0300

    update with Ajax

--- js/ajax.js
+++
@@ -1,9 +0,0 @@
-function GetXMLHttp(){
-	if(navigator.appName == "Microsoft Internet Explore"){
-		xmlHttp = new ActiveXObject(Microsoft.XMLHTTP);
-	}else{
-		xmlHttp = new new XMLHttpRequest();
-	}
-	return xmlHttp;
-}
-var XMLHttpRequest = GetXMLHttp();
